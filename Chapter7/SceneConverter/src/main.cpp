61ceae53078df45b0eb12ad6903234c5ab30dfdf
zoloypzuo <910181598@qq.com>
2021-12-14 19:00:17 +0800

    Update main.cpp

--- Chapter7/SceneConverter/src/main.cpp
+++ Chapter7/SceneConverter/src/main.cpp
@@ -19,9 +19,11 @@
 #include "shared/scene/Scene.h"
 #include "shared/scene/MergeUtil.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-#include "stb_image.h"
+#include <stb/stb_image_write.h>
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
 
 #include <meshoptimizer.h>
